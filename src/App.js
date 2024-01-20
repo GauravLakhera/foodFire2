c751d449fa59ae0111a07bf2bd9c57dc9dc195d3
gaurav lakhera <lakhera2525@gmail.com>
2024-01-20 14:21:50 +0530

    Imported outled from the react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import Body from "./components/Body";
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
-import AboutUs from "./components/AboutUs"
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import AboutUs from "./components/AboutUs";
 import ConnectUs from "./components/ConnectUs";
 import Error from "./components/Error";
 
-
 //Default import=>
-import HeadComponent from "./components/Header";//imorting the default import from Header.js
+import HeadComponent from "./components/Header"; //imorting the default import from Header.js
 
 //Named import=>
 // import { Logo,Tital } from "./components/Header";//importing using the name of variable
-
 
 /**
  * Header
@@ -39,32 +37,34 @@ const AppLayout = () => {
   return (
     <>
       <HeadComponent />
-      <Body />
+      <Outlet/>
       <Footer />
     </>
   );
 };
-
-const appRouter= createBrowserRouter([
+//Imported outled from the react-router-dom and added the childer to main rout 
+const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <AppLayout/>,
-    errorElement : <Error/>
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <AboutUs />,
+      },
+      {
+        path: "/contact",
+        element: <ConnectUs />,
+      },
+    ],
+    
   },
-  {
-    path: "/about",
-    element: <AboutUs/>,
-    errorElement : <Error/>
-  },
-  {
-    path:"/contact",
-    element : <ConnectUs/>,
-    errorElement : <Error/>
-  }
-]
-)
-
-
+]);
 
 const root = createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={appRouter}/> );
+root.render(<RouterProvider router={appRouter} />);
